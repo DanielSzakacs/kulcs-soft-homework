d525c344ab3fa612862d2e107faf1c0e8f3318da
danielszakacs <daniel.sokach@gmail.com>
2019-02-21 16:33:05 +0100

    added all other basic file to  git.

---
+++ src/main/java/com/daniel/szakacs/kulcssofthomework/KulcssofthomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.szakacs.kulcssofthomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KulcssofthomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KulcssofthomeworkApplication.class, args);
+    }
+
+}
