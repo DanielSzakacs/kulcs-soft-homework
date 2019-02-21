ae8db15c428cfaeec464fbcf224ae1fb995e8431
danielszakacs <daniel.sokach@gmail.com>
2019-02-21 17:25:04 +0100

    Login and Security methods are done

--- src/main/java/com/daniel/szakacs/kulcssofthomework/service/login/Login.java
+++ src/main/java/com/daniel/szakacs/kulcssofthomework/service/login/Login.java
@@ -1,4 +1,32 @@
 package com.daniel.szakacs.kulcssofthomework.service.login;
 
+import com.daniel.szakacs.kulcssofthomework.service.module.User;
+import com.daniel.szakacs.kulcssofthomework.service.repository.Userrepository;
+import com.daniel.szakacs.kulcssofthomework.service.security.SecurityManger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.security.InvalidAlgorithmParameterException;
+
 public class Login {
+
+    @Autowired
+    private SecurityManger securityManger;
+
+    // TODO ez a fő method
+    public boolean isUserEmailAndPasswordCorrect(String email, String password, Userrepository userRepository) {
+        if(userRepository.existsByEmail(email)){
+            return isPasswordCorrect(email, password, userRepository);
+        }else{
+            new InvalidAlgorithmParameterException("Your email is not correct");
+        }
+        return false;
+    }
+
+    public boolean isPasswordCorrect(String email, String password, Userrepository userRepository){
+        User user = userRepository.getByEmail(email);
+        String userHashedPassword = user.getPassword();
+        return securityManger.matchPasswords(password, userHashedPassword);
+    }
+
+
 }
--- src/main/java/com/daniel/szakacs/kulcssofthomework/service/security/SecurityManger.java
+++ src/main/java/com/daniel/szakacs/kulcssofthomework/service/security/SecurityManger.java
@@ -1,19 +1,22 @@
 package com.daniel.szakacs.kulcssofthomework.service.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class Secutiry {
+public class SecurityManger {
 
     private PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
     public String hashingCode(String password){
-        return this.passwordEncoder().encode(password);
+        return passwordEncoder().encode(password);
     }
 
+    @Bean
     public boolean matchPasswords(String password, String hashedPassword){
-        return this.passwordEncoder().matches(password, hashedPassword);
+        return passwordEncoder().matches(password, hashedPassword);
     }
 }
